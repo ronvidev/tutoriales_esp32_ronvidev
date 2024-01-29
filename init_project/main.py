7c89b9d04c1867d4d9416067ffc4e51c9043d51b
RonWizLee <69706464+RonWizLee@users.noreply.github.com>
2024-01-28 19:18:27 -0500

    fix esp32

--- init_project/main.py
+++ init_project/main.py
@@ -1,21 +1,10 @@
-# main.py -- put your code here!
 from machine import Pin
 import time
 
-# Configura el pin GPIO donde está conectado el LED
-pin_led = Pin(2, Pin.OUT)  # Pin 2 en el ESP32
+pin_led = Pin(2, Pin.OUT)
 
-# Función para encender el LED
-def encender_led():
-    pin_led.value(1)  # Establece el pin en HIGH para encender el LED
-
-# Función para apagar el LED
-def apagar_led():
-    pin_led.value(0)  # Establece el pin en LOW para apagar el LED
-
-# Ciclo principal
 while True:
-    encender_led()  # Enciende el LED
-    time.sleep(1)   # Espera 1 segundo
-    apagar_led()    # Apaga el LED
-    time.sleep(1)   # Espera 1 segundo
+    pin_led.value(1)
+    time.sleep(1) 
+    pin_led.value(0)
+    time.sleep(1) 
